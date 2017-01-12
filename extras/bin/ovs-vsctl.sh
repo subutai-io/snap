c3989f1c2f9c309a4ad5dc9b64908db219d9063f
tzununbekov <t.zununbekov@gmail.com>
2017-01-12 15:47:55 +0600

    Trash removed

--- extras/bin/ovs-vsctl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#Include enviroment variables
-. $(cd `dirname "${BASH_SOURCE[0]}"` && pwd)/subutai.env
-
-$SUBUTAI_APP_PREFIX/bin/ovs-vsctl --db=unix:$SUBUTAI_DATA_PREFIX/ovs/db.sock "$@"
