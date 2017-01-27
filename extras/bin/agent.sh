3299539542a556e60affed1c4078c61478fbe6b9
tzununbekov <t.zununbekov@gmail.com>
2017-01-27 17:42:18 +0600

    Ssh key permissions fix

--- extras/bin/agent.sh
+++ extras/bin/agent.sh
@@ -2,6 +2,9 @@
 #Include enviroment variables
 . $(cd `dirname "${BASH_SOURCE[0]}"` && pwd)/subutai.env
 
+cp $SUBUTAI_APP_PREFIX/etc/ssh.pem $SUBUTAI_DATA_PREFIX
+chmod 600 $SUBUTAI_DATA_PREFIX/ssh.pem
+
 while [ ! -S /sys/fs/cgroup/cgmanager/sock ]; do
 	sleep 1
 done
