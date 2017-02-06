2522a18fd6d47051bc0713d4beefac2fec51eda6
Dmitry Shihovtsev <soffokulus@gmail.com>
2017-02-06 16:36:23 +0600

    Fixed broken nginx in subutai-dev snap package. #15

--- extras/bin/nginx.sh
+++ extras/bin/nginx.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 mkdir -p $SNAP_DATA/../common/cache/nginx/
+mkdir -p $SNAP_DATA/nginx/conf.d/
 mkdir -p $SNAP_DATA/nginx/cache/
 mkdir -p $SNAP_DATA/nginx/log/
 mkdir -p $SNAP_DATA/nginx/run/
 mkdir -p $SNAP_DATA/web/ssl/
+
+sed -e "s|/snap/subutai/|/snap/$SNAP_NAME/|g" $SNAP/etc/nginx/proxy.conf > $SNAP_DATA/nginx/conf.d/proxy.conf
+
 while [ $(ping 8.8.8.8 -c1 | grep -c "1 received") -ne 1 ]; do
         sleep 1
 done
