822daf0a18914c8f78182c17eafea59ce56865b9
Dmitry Shihovtsev <soffokulus@gmail.com>
2017-02-08 11:39:30 +0600

    Fixed branch in agent config

--- extras/bin/agent.sh
+++ extras/bin/agent.sh
@@ -5,7 +5,7 @@ chmod 600 $SNAP_DATA/ssh.pem
 if [ ! -f $SNAP_DATA/agent.gcfg ]; then
 	BRANCH=$(echo ${SNAP_NAME#subutai} | tr -d '-')
 	sed -e "s|branch = dev|branch = $BRANCH|g" $SNAP/etc/agent.gcfg > $SNAP_DATA/agent.gcfg
-	sed -e "s|/snap/subutai/|/snap/$SNAP_NAME/|g" $SNAP/etc/agent.gcfg > $SNAP_DATA/agent.gcfg
+	sed -e "s|/snap/subutai/|/snap/$SNAP_NAME/|g" -i $SNAP_DATA/agent.gcfg
 fi
 
 while [ ! -S /sys/fs/cgroup/cgmanager/sock ]; do
