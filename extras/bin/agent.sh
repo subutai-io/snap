cc9ccf86d2db59383c32440a9019914cfc580d31
Dmitry Shihovtsev <soffokulus@gmail.com>
2017-03-02 17:18:26 +0600

    Migrated to devcdn

--- extras/bin/agent.sh
+++ extras/bin/agent.sh
@@ -4,12 +4,6 @@ chmod 600 $SNAP_DATA/ssh.pem
 
 resize2fs /dev/sda3
 
-if [ ! -f $SNAP_DATA/agent.gcfg ]; then
-	BRANCH=$(echo ${SNAP_NAME#subutai} | tr -d '-')
-	sed -e "s|branch = dev|branch = $BRANCH|g" $SNAP/etc/agent.gcfg > $SNAP_DATA/agent.gcfg
-	sed -e "s|/snap/subutai/|/snap/$SNAP_NAME/|g" -i $SNAP_DATA/agent.gcfg
-fi
-
 while [ ! -S /sys/fs/cgroup/cgmanager/sock ]; do
 	sleep 1
 done
