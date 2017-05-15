33895cf8c9cb4a18a724fe5c84818b883cceca15
tzununbekov <t.zununbekov@gmail.com>
2017-05-15 16:41:22 +0600

    DHCP wrapper fix

--- extras/bin/dhcpd.sh
+++ extras/bin/dhcpd.sh
@@ -3,5 +3,5 @@ while [ "`ifconfig | grep '10.10.10.254' | wc -l`" -eq "0" ]; do
         sleep 1
 done
 	  
-$SNAP/bin/dnsmasq -k -p 0 --interface=nat --dhcp-range=10.10.10.10,10.10.10.240,12h --dhcp-leasefile=$SNAP_DATA/dnsmasq.leases --dhcp-option=6,10.10.10.254
+$SNAP/bin/dnsmasq -k -p 0 --interface=lxc-br --dhcp-range=10.10.10.10,10.10.10.240,12h --dhcp-leasefile=$SNAP_DATA/dnsmasq.leases --dhcp-option=6,10.10.10.254
 
