95911ce17bfb140249d420ae76d487409f27749b
Aron Xu <happyaron.xu@gmail.com>
2018-02-23 01:28:04 +0800

    Change pid file of dnsmasq to $SNAP_DATA

--- extras/bin/dhcpd.sh
+++ extras/bin/dhcpd.sh
@@ -3,5 +3,4 @@ while [ "`/bin/ip addr show | grep -c 10.10.10.254`" == "0" ]; do
         sleep 1
 done
 	  
-$SNAP/bin/dnsmasq -k -p 0 --interface=lxc-br --dhcp-range=10.10.10.10,10.10.10.240,12h --dhcp-leasefile=$SNAP_DATA/dnsmasq.leases --dhcp-option=6,10.10.10.254
-
+$SNAP/bin/dnsmasq -k -p 0 --pid-file=$SNAP_DATA/dnsmasq.pid --interface=lxc-br --dhcp-range=10.10.10.10,10.10.10.240,12h --dhcp-leasefile=$SNAP_DATA/dnsmasq.leases --dhcp-option=6,10.10.10.254
